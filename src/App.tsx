3fce4b8bd8c024d9ca1265b5115739735eeecc3f
Esthevan <esthevanferretti69@gmail.com>
2022-09-30 23:00:10 -0300

    moved over to vite

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import { Canvas } from '@react-three/fiber'
+import { Sky } from '@react-three/drei'
+
+function App() {
+
+  return (
+    <>
+      <Canvas>
+        <Sky></Sky>
+      </Canvas>    
+    </>
+  )
+}
+
+export default App

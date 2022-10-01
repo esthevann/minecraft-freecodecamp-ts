e14f3f7d39b2274317bbe66d4263c84347f788e4
Esthevan <esthevanferretti69@gmail.com>
2022-10-01 19:17:58 -0300

    defined textures

---
+++ src/images/textures.ts
@@ -0,0 +1,19 @@
+import { dirtImg, glassImg, logImg, grassImg, woodImg } from './images'
+
+import { TextureLoader } from 'three';
+
+const dirtTexture = new TextureLoader().load(dirtImg);
+const logTexture = new TextureLoader().load(logImg);
+const grassTexture = new TextureLoader().load(grassImg);
+const glassTexture = new TextureLoader().load(glassImg);
+const woodTexture = new TextureLoader().load(woodImg);
+const groundTexture = new TextureLoader().load(grassImg);
+
+export {
+    dirtTexture,
+    logTexture,
+    grassTexture,
+    glassTexture,
+    woodTexture,
+    groundTexture,
+}

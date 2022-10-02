b01d7888a443c385c82fe3c8d367ceb958a7615b
Esthevan <esthevanferretti69@gmail.com>
2022-10-01 21:06:02 -0300

    ground component

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,7 @@
 import { Canvas } from '@react-three/fiber'
 import { Sky } from '@react-three/drei'
+import { Physics } from '@react-three/cannon'
+import {Ground} from './components/Ground';
 
 function App() {
 
@@ -7,6 +9,10 @@ function App() {
     <>
       <Canvas>
         <Sky sunPosition={[100, 100,20]} />
+        <ambientLight intensity={0.5} />
+        <Physics>
+          <Ground />
+        </Physics>
       </Canvas>    
     </>
   )
---
+++ src/components/Ground.tsx
@@ -0,0 +1,22 @@
+import { usePlane } from "@react-three/cannon"
+import { RepeatWrapping } from "three";
+import { groundTexture } from "../images/textures";
+export const Ground = () => {
+    let [ref] = usePlane(() => ({
+        rotation: [-Math.PI / 3, 0, 0],
+        position: [0, 0, 0],
+    
+    }));
+
+
+    groundTexture.wrapS = RepeatWrapping;
+    groundTexture.wrapT = RepeatWrapping;
+    groundTexture.repeat.set( 100, 100 );
+    return  (
+        //@ts-ignore
+        <mesh ref={ref}>
+            <planeBufferGeometry attach="geometry" args={[100,100]} />	
+            <meshStandardMaterial attach="material" map={groundTexture}  />
+        </mesh>
+    )
+}
